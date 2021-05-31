e1ada783f95876a6a4388cf16794f841804e9863
Breno Lopes do Carmo <brenolcdev@gmail.com>
2021-05-31 02:15:49 -0300

    first commit

---
+++ Assets/main-js/script.js
@@ -0,0 +1,8 @@
+// SCRIPT MQN ESCREVER BANNER
+
+var typed = new Typed (".typing", {
+    strings: ["Powerful", "Incredible", "Unique","Ruthless" ,"The solution" ],
+    typeSpeed: 75,
+    backSpeed: 60,
+    loop: true
+});
